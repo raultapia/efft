33d43a19406a01a652b21dd70cf77bffeec41757
Raul Tapia <raultapia@us.es>
2025-10-09 15:07:28 +0200

    Update benchmarks to use size_t and adjust packet args

--- benchmarks/efft_benchmark.cpp
+++ benchmarks/efft_benchmark.cpp
@@ -1,8 +1,7 @@
 #include "efft.hpp"
 #include <benchmark/benchmark.h>
+#include <cstddef>
 #include <random>
-
-constexpr unsigned int NTEST = 250;
 
 template <unsigned int N>
 class RandEventGenerator {
@@ -26,80 +25,86 @@ private:
 
 template <unsigned int FRAME_SIZE>
 static void BenchmarkFeedWithEventsFFTW(benchmark::State &state) {
+  constexpr std::size_t num_events_to_process = 250;
   eFFT<FRAME_SIZE> efft;
   efft.initializeGroundTruth();
   RandEventGenerator<FRAME_SIZE> rand;
 
   Stimulus s;
   for(auto _ : state) {
-    for(unsigned int test = 0; test < NTEST; test++) {
+    for(std::size_t it = 0; it < num_events_to_process; it++) {
       s = rand.next();
       efft.updateGroundTruth(s);
       [[maybe_unused]] auto result = efft.getGroundTruthFFT();
     }
   }
 }
-BENCHMARK_TEMPLATE(BenchmarkFeedWithEventsFFTW, 16);
-BENCHMARK_TEMPLATE(BenchmarkFeedWithEventsFFTW, 32);
-BENCHMARK_TEMPLATE(BenchmarkFeedWithEventsFFTW, 64);
-BENCHMARK_TEMPLATE(BenchmarkFeedWithEventsFFTW, 128);
-BENCHMARK_TEMPLATE(BenchmarkFeedWithEventsFFTW, 256);
+// BENCHMARK_TEMPLATE(BenchmarkFeedWithEventsFFTW, 16);
+// BENCHMARK_TEMPLATE(BenchmarkFeedWithEventsFFTW, 32);
+// BENCHMARK_TEMPLATE(BenchmarkFeedWithEventsFFTW, 64);
+// BENCHMARK_TEMPLATE(BenchmarkFeedWithEventsFFTW, 128);
+// BENCHMARK_TEMPLATE(BenchmarkFeedWithEventsFFTW, 256);
 
 template <unsigned int FRAME_SIZE>
 static void BenchmarkFeedWithPacketsFFTW(benchmark::State &state) {
+  constexpr std::size_t num_events_to_process = 500000;
+  const std::size_t num_iterations = num_events_to_process / state.range(0);
   eFFT<FRAME_SIZE> efft;
   efft.initializeGroundTruth();
   RandEventGenerator<FRAME_SIZE> rand;
 
   Stimuli ss;
   for(auto _ : state) {
-    for(unsigned int test = 0; test < NTEST; test++) {
+    for(std::size_t it = 0; it < num_iterations; it++) {
       ss = rand.next(state.range(0));
       efft.updateGroundTruth(ss);
       [[maybe_unused]] auto result = efft.getGroundTruthFFT();
     }
   }
 }
-BENCHMARK_TEMPLATE(BenchmarkFeedWithPacketsFFTW, 128)->Arg(1000)->Arg(5000)->Arg(10000)->Arg(50000)->Arg(100000);
-BENCHMARK_TEMPLATE(BenchmarkFeedWithPacketsFFTW, 256)->Arg(1000)->Arg(5000)->Arg(10000)->Arg(50000)->Arg(100000);
+BENCHMARK_TEMPLATE(BenchmarkFeedWithPacketsFFTW, 128)->Arg(100)->Arg(500)->Arg(1000)->Arg(2500)->Arg(5000);
+BENCHMARK_TEMPLATE(BenchmarkFeedWithPacketsFFTW, 256)->Arg(100)->Arg(500)->Arg(1000)->Arg(2500)->Arg(5000);
 
 template <unsigned int FRAME_SIZE>
 static void BenchmarkFeedWithEvents(benchmark::State &state) {
+  constexpr std::size_t num_events_to_process = 250;
   eFFT<FRAME_SIZE> efft;
   efft.initialize();
   RandEventGenerator<FRAME_SIZE> rand;
 
   Stimulus s;
   for(auto _ : state) {
-    for(unsigned int test = 0; test < NTEST; test++) {
+    for(std::size_t it = 0; it < num_events_to_process; it++) {
       s = rand.next();
       efft.update(s);
       [[maybe_unused]] auto result = efft.getFFT();
     }
   }
 }
-BENCHMARK_TEMPLATE(BenchmarkFeedWithEvents, 16);
-BENCHMARK_TEMPLATE(BenchmarkFeedWithEvents, 32);
-BENCHMARK_TEMPLATE(BenchmarkFeedWithEvents, 64);
-BENCHMARK_TEMPLATE(BenchmarkFeedWithEvents, 128);
-BENCHMARK_TEMPLATE(BenchmarkFeedWithEvents, 256);
+// BENCHMARK_TEMPLATE(BenchmarkFeedWithEvents, 16);
+// BENCHMARK_TEMPLATE(BenchmarkFeedWithEvents, 32);
+// BENCHMARK_TEMPLATE(BenchmarkFeedWithEvents, 64);
+// BENCHMARK_TEMPLATE(BenchmarkFeedWithEvents, 128);
+// BENCHMARK_TEMPLATE(BenchmarkFeedWithEvents, 256);
 
 template <unsigned int FRAME_SIZE>
 static void BenchmarkFeedWithPackets(benchmark::State &state) {
+  constexpr std::size_t num_events_to_process = 500000;
+  const std::size_t num_iterations = num_events_to_process / state.range(0);
   eFFT<FRAME_SIZE> efft;
   efft.initialize();
   RandEventGenerator<FRAME_SIZE> rand;
 
   Stimuli ss;
   for(auto _ : state) {
-    for(unsigned int test = 0; test < NTEST; test++) {
+    for(std::size_t it = 0; it < num_iterations; it++) {
       ss = rand.next(state.range(0));
       efft.update(ss);
       [[maybe_unused]] auto result = efft.getFFT();
     }
   }
 }
-BENCHMARK_TEMPLATE(BenchmarkFeedWithPackets, 128)->Arg(1000)->Arg(5000)->Arg(10000)->Arg(50000)->Arg(100000);
-BENCHMARK_TEMPLATE(BenchmarkFeedWithPackets, 256)->Arg(1000)->Arg(5000)->Arg(10000)->Arg(50000)->Arg(100000);
+BENCHMARK_TEMPLATE(BenchmarkFeedWithPackets, 128)->Arg(100)->Arg(500)->Arg(1000)->Arg(2500)->Arg(5000);
+BENCHMARK_TEMPLATE(BenchmarkFeedWithPackets, 256)->Arg(100)->Arg(500)->Arg(1000)->Arg(2500)->Arg(5000);
 
 BENCHMARK_MAIN();
